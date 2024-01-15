df5f21c2e28faf189cea80ef391b93f849cab795
Tye Peck <tye@novatasolutions.com.au>
2024-01-15 13:48:21 +0800

    perf: add types

--- index.d.ts
+++ index.d.ts
@@ -7,10 +7,49 @@ export type Enrollment = {
   secret: string;
   deviceIdentifier: string;
   deviceName: string;
-  deviceGCMToken: string;
   deviceToken: string;
-  privateKey: string;
 };
+
+export type Aps = {
+  alert: ApsAlert;
+  category: string;
+};
+
+export type ApsAlert = {
+  body: string;
+  title: string;
+};
+
+export type MfaPayload = {
+  dai?: string;
+  txtkn?: string;
+  txlnkid?: string;
+  c?: string;
+  d?: string;
+  sh?: string;
+  s?: string;
+  b?: VersionName;
+  os?: VersionName;
+  l?: Location;
+};
+
+export type VersionName = {
+  n?: string;
+  v?: string;
+};
+
+export type Location = {
+  lat?: string;
+  lng?: string;
+  n?: string;
+};
+
+export type NotificationDataIos = {
+  aps: Aps;
+  mfa: MfaPayload;
+};
+export type NotificationDataAndroid = MfaPayload;
+
 declare namespace Auth0Guardian {
   export function initialize(auth0Domain: string): Promise<boolean>;
 
@@ -25,13 +64,13 @@ declare namespace Auth0Guardian {
 
   export function getTOTP(enrollmentId: string): Promise<string>;
 
-  export function allow(notificationData: {
-    [key: string]: string;
-  }): Promise<boolean>;
+  export function allow(
+    notificationData: NotificationDataIos | NotificationDataAndroid,
+  ): Promise<boolean>;
 
-  export function reject(notificationData: {
-    [key: string]: string;
-  }): Promise<boolean>;
+  export function reject(
+    notificationData: NotificationDataIos | NotificationDataAndroid,
+  ): Promise<boolean>;
 }
 
 export default Auth0Guardian;
